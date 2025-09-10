20d6740998d56329fb3562377cbc53a28a88a1ba
Sijan-Giri <girisijan346@gmail.com>
2025-09-10 16:20:21 +0545

    feat : UI completed

--- src/components/UI/UseCase01.tsx
+++ src/components/UI/UseCase01.tsx
@@ -5,21 +5,19 @@ const UseCase01 = () => {
 
       <div className="relative z-10 w-full padding-common text-white">
         <div className="grid gap-6 lg:grid-rows-[auto_auto_1fr] lg:min-h-[450px] lg:max-w-full">
-          {/* Heading */}
+
           <div className="flex-center-start">
             <h2 className="text-lg sm:text-xl lg:text-2xl xl:text-3xl font-bold leading-tight text-center lg:text-left">
               USE CASE 01 – Master Data Management
             </h2>
           </div>
 
-          {/* Description */}
           <div className="flex-center-end">
             <p className="text-sm sm:text-base lg:text-lg xl:text-xl leading-relaxed max-w-3xl text-center lg:text-right">
               The CODEX system will push master data to other applications by using API gateway.
             </p>
           </div>
 
-          {/* Image */}
           <div className="flex justify-center">
             <img
               src="/src/assets/UseCase01-img.png"
